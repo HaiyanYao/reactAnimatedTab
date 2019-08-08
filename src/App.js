3eb67b0fbc50c924153597d7327ef1130b1e097c
HaiyanYao <haiyanyao1988@gmail.com>
2019-08-07 20:16:05 -0400

    tabs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Tabs from './Tabs/Tabs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const styles = {
+  fontFamily: 'sans-serif',
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: 'aTab'};
+  }
+  render() {
+    const content = {
+      aTab: 'Tab A',
+      bTab: 'Tab B',
+      cTab: 'Tab C',
+    };
+    return (
+      <div style={styles} style={{textAlign:"center"}}>
+        <h1 style={{textAlign:"center"}}>Tabs Example</h1>
+        <div style={{textAlign:"center",width:"999px",border:"1px solid rgb(6, 33, 107)",height:"500px"}}>
+        <Tabs
+          active={this.state.active}
+          onChange={active => this.setState({active})}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <div key="aTab">A Title</div>
+          <div key="bTab">B Very Long Title</div>
+          <div key="cTab">{this.state.active === 'bTab' ? 'Title' : 'Very Long Title'}</div>
+        </Tabs>
+        <h2>Content</h2>
+        <p>{content[this.state.active]}</p>
+      </div>
+      </div>
 
+    );
+  }
+};
 export default App;
